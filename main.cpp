55cc6a9b3d70aed663c5680958b353e7c7cdbc84
Shehtab Zaman <shehtabzaman@gmail.com>
2018-03-08 16:21:18 -0500

    Completed 2 bit modal test case

--- main.cpp
+++ main.cpp
@@ -41,11 +41,15 @@ int main(int argc, char *argv[]) {
     /*
     One-bit bimodal test with table size 16
     */
+
+
 	unsigned long address;
   int index;
   vector<bool> table (32,true);
   bool result;
   int correct = 0;
+
+
   trace_file.open(argv[1]);
 	if(trace_file.is_open()){
 		while(getline(trace_file,line)){
@@ -62,11 +66,56 @@ int main(int argc, char *argv[]) {
         }
       }
 		}
+    trace_file.close();
 	}
   output_file.open("OneBitBimodal.txt", ios::trunc);
   output_file <<"Input file name: "<< argv[1] << '\n';
   output_file <<correct << "," << notTaken + taken <<";"<<'\n';
   output_file.close();
+
+  /*
+  2 bit BiModal
+  */
+  correct = 0;
+  vector<int> twoBitTable (2048,3);
+  trace_file.open(argv[1]);
+  if(trace_file.is_open()){
+
+    while(getline(trace_file,line)){
+      address = stoul(line.substr(0,10),nullptr, 16);
+      index = address % 2048;
+      result = (line[11] == 'T');
+
+      if(twoBitTable[index] > 1 && result){
+        correct ++;
+        if(twoBitTable[index] == 2){
+          twoBitTable[index]++;
+        }
+      }
+      else if(twoBitTable[index]> 1 && !result){
+        twoBitTable[index]--;
+      }
+      else if(twoBitTable[index] < 2 && !result){
+        correct++;
+        if(twoBitTable[index] == 1){
+          twoBitTable[index] --;
+        }
+      }else{
+        twoBitTable[index]++;
+      }
+
+    }
+  trace_file.close();
+  }
+
+  output_file.open("TwoBitBimodal.txt", ios::trunc);
+  output_file <<"Input file name: "<< argv[1] << '\n';
+  output_file <<correct << "," << notTaken + taken <<";"<<'\n';
+  output_file.close();
+  cout << correct << '\n';
+  /*
+  G share
+  */
   }
   return 0;
 }
